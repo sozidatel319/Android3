e2aec1e4acde11a7e65e1017c4372b7e72d95505
sozidatel319 <mbogat@yandex.ru>
2020-01-17 17:04:12 +0300

    Сделал список юзеров(RecyclerView)

---
+++ app/src/main/java/com/example/myapplication/user/list/UsersAdapter.java
@@ -0,0 +1,48 @@
+package com.example.myapplication.user.list;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.myapplication.R;
+
+import java.util.List;
+
+public class UsersAdapter extends RecyclerView.Adapter<UsersAdapter.UserViewHolder> {
+    private List<User> users;
+
+    public UsersAdapter(List<User> users) {
+        this.users = users;
+    }
+
+    @NonNull
+    @Override
+    public UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new UserViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_user,parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserViewHolder holder, int position) {
+    holder.name.setText(users.get(position).getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+
+    public class UserViewHolder extends RecyclerView.ViewHolder{
+    TextView name;
+    ImageView avatar;
+        public UserViewHolder(@NonNull View itemView) {
+            super(itemView);
+            name = itemView.findViewById(R.id.text_name);
+            avatar = itemView.findViewById(R.id.image_avatar);
+        }
+    }
+}
